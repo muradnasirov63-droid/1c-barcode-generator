f77bb36e90338958d4c7efe94d83d910dfa2d140
Pragmatic Programmer <prag_prog@gmail.com>
2026-03-29 16:50:16 +0300

    исправление формата штрихкода

--- Barcode(new).cpp
+++ Barcode(new).cpp
@@ -1,28 +1,34 @@
 ﻿#include <iostream>
-#include <fstream>
-#include "DataMatrixEncoder.h"
-#include "BitMatrix.h"
+#include <fstream> // Работа с файлами 
+#include "DataMatrixEncoder.h" // DataMatrix
+#include "BitMatrix.h" // Хранятся нули и единицы шртихкода
+
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "RU");
     try {
-        DataMatrixEncoder myEncoder;
-        auto matrix = myEncoder.encode(L"MURAD_PROFESSIONAL", 200, 200);
-        ofstream out("final_modular_dm.pbm");
+        DataMatrixEncoder myEncoder; // Создаём объект-кодировщик
+        auto matrix = myEncoder.encode(L"THIS_IS_KIMPINTYAO", 500, 500); // Превращаем в сетку из точек
+
+        ofstream out("final_barcode.svg"); // Создание файла 
         if (out) {
-            out << "P1\n" << matrix.width() << " " << matrix.height() << "\n";
-            for (int y = 0; y < matrix.height(); ++y) {
-                for (int x = 0; x < matrix.width(); ++x) {
-                    out << (matrix.get(x, y) ? "1 " : "0 ");
+            out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; // Это паспорт файла. Формат SVG основан на XML
+            out << "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"400\" height=\"400\" viewBox=\"0 0 " // Ссылка на словарь правил. Квадрат 400*400
+                << matrix.width() << " " << matrix.height() << "\">\n"; // Реальное колиство клеток в шртрихкоде
+            out << "<rect width=\"100%\" height=\"100%\" fill=\"white\"/>\n"; // Белый цвет фона заднего вроде
+            out << "<path d=\""; // Путь
+            for (int y = 0; y < matrix.height(); ++y) { // Выбор строки 
+                for (int x = 0; x < matrix.width(); ++x) { // Выбор клетки внутри строки
+                    if (matrix.get(x, y)) out << "M" << x << "," << y << "h1v1h-1z "; // Дложна ли быть точка черная
                 }
-                out << "\n";
             }
-            cout << "ВСЁ ГОТОВО! Файл 'final_modular_dm.pbm' создан." << endl;
+            out << "\" fill=\"black\"/>\n</svg>";
+            cout << "УСПЕХ! Файл 'final_barcode.svg' создан." << endl;
         }
     }
     catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
+        cerr << "Ошибка братиш, че делать будешь: " << e.what() << endl;
     }
     cout << "Нажми Enter...";
     cin.get();
